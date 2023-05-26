85979f3f2c318ebcb7b911cf522736c951acceac
MahirSiam <mahir.siam024@gmail.com>
2023-05-26 17:18:22 +0600

    introduction part

---
+++ home01.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homepage01</title>
+    <link rel="stylesheet" href="./home01 style.css">
+<body>
+    <div class="container">
+        <nav>
+      <div class="nav-logo">
+        <img src="./logo.png">
+
+      </div>
+      <div class="nav-links">
+        <a href="./About Us.php" class="bto">ABOUT US</a>
+        <div>
+            <a href="../buttons/login_op.php" class="btm">LOGIN</a>
+            <a href="../buttons/reg_op.php" class="btn">SIGN UP</a>
+        </div>
+      </div>
+    </nav>
+    <div class="content">
+        <h1>An Educated Mind  <br> Can Teach Many</h1>
+        <div class="search-box">
+            <input type="text" placeholder="Search for any course">
+            <button type="submit" ><img src="./search.png"></button>
+        </div>
+        <p>Millons of courses to search from</p>
+    </div>
+
+    </div>
+</body>
+</html>
